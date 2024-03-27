344f5134cbaac082d6191823ef7a5c8cae23c5a6
Ujer123 <ujermohd0@gmail.com>
2024-03-28 02:50:28 +0530

    hello

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-$(".roundButton").click(function(){
+$(".toggle").click(function(){
     $("h1, h2, p, h4, .catalog, a, h6,").toggleClass("textcl");
-    $("body").toggleClass("change-theme");
+    $("body").toggleClass("bg");
     });
+
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
+    })
