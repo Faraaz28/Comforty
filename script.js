42edb4f85e9f320087910e19e9da1a38fe3ca19e
Faraaz.28 <faraazansari582@gmail.com>
2024-03-28 17:10:33 +0530

    Final

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-$(".toggle").click(function(){
-    $("h1, h2, p, h4, .catalog, a, h6,").toggleClass("textcl");
-    $("body").toggleClass("bg");
+$(".roundButton").click(function(){
+    $("h1, h2, h3, svg, h5, p, h4, .catalog, a, h6, .owl-theme").toggleClass("textcol");
+    $("body, .card, .cardd-border").toggleClass("bg");
     });
 
-    $('.owl-carousel').owlCarousel({
-        loop: true,
-        margin: 10,
-        nav: true,
-        responsive: {
-            0: {
-                items: 1
-            },
-            600: {
-                items: 3
-            },
-            1000: {
-                items: 5
-            }
-        }
-    })
+    // $('.owl-carousel').owlCarousel({
+    //     loop: true,
+    //     margin: 10,
+    //     nav: true,
+    //     responsive: {
+    //         0: {
+    //             items: 1
+    //         },
+    //         600: {
+    //             items: 3
+    //         },
+    //         1000: {
+    //             items: 5
+    //         }
+    //     }
+    // })
