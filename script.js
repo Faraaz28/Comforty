cfc8b67d16503c211b1e56b55ebe8e836fc117be
Faraaz.28 <faraazansari582@gmail.com>
2024-04-14 13:28:59 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+$(".roundButton").click(function(){
+    $("h1, h2, h3, svg, h5, p, h4, .cdborder, .catalog, a, h6, .owl-theme").toggleClass("textcol");
+    $("body, .card,  .cardd-border").toggleClass("bg");
+});
