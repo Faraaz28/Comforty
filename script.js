ae3bc0408416caaf2d2bb0d45d001ec350d44a8b
Faraaz.28 <faraazansari582@gmail.com>
2024-04-21 19:55:19 +0530

    HEY

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 $(".roundButton").click(function(){
-    $("h1, h2, h3, svg, h5, p, h4, .cdborder, .catalog, a, h6, .owl-theme").toggleClass("textcol");
+    $("h1, h2, h3, svg, h5, p, h4, .cdborder, .catalog, a, .item, h6, .owl-theme").toggleClass("textcol");
     $("body, .card,  .cardd-border").toggleClass("bg");
 });
